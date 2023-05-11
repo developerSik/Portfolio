40752dd061f15d53596d3dec22d4d5be5421f80a
dev-quokka <rlaehdcks1010@gmail.com>
2023-05-11 16:48:18 +0900

    qkqh

---
+++ src/main/java/com/example/tikkle/TikkleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tikkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikkleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TikkleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tikkle/TikkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tikkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
