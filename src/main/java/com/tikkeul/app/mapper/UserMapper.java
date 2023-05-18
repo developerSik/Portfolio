bb5dc78afedba9fe825d20bf4e78df9ee75fdc11
developerSik <jung9841@gmail.com>
2023-05-18 15:29:22 +0900

    Config type alias 수정

--- src/main/java/com/tikkeul/app/mapper/UserMapper.java
+++ src/main/java/com/tikkeul/app/mapper/UserMapper.java
@@ -1,7 +1,14 @@
 package com.tikkeul.app.mapper;
 
+import com.tikkeul.app.domain.vo.UserVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
+//  관리자페이지에서 회원 리스트 조회
+    public List<UserVO> adminSelectUserAll();
+
+
 }
