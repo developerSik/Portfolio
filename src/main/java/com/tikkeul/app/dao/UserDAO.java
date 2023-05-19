8befe9643e9788521945e589189a835d49fb56a7
YunkiHong <>
2023-05-19 17:53:30 +0900

    05-18 회원가입 백엔드 작업중

--- src/main/java/com/tikkeul/app/dao/UserDAO.java
+++ src/main/java/com/tikkeul/app/dao/UserDAO.java
@@ -5,6 +5,7 @@ import com.tikkeul.app.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Optional;
 
@@ -25,29 +26,14 @@ public class UserDAO {
     };
 
     //    회원가입
-    public void insert(UserVO userVO){
+    public RedirectView insert(UserVO userVO){
         userMapper.insert(userVO);
+        return new RedirectView("/login");
     };
 
     //    로그인
     public Optional<Long> selectByUserIdAndUserPassword(@Param("id") String id, @Param("password") String password){
         return userMapper.selectByUserIdAndUserPassword(id,password);
-    };<select id="adminSelectUserAll" resultType="userVO">
-        SELECT ID, NAME , IDENTIFICATION, ADDRESS, PHONENUMBER
-        FROM USERS
-    </select>
-
-    <select id="selectByUserId" resultType="UserVO">
-    SELECT ID, IDENTIFICATION, PASSWORD, NAME, PHONENUMBER, ADDRESS, STATUS, ROLL, CREATEDDATE, PROFILEIMAGENAME, PROFILEIMAGESIZE, PROFILEIMAGEUUID, SAVINGLEVEL_ID
-    FROM USERS
-
-    </select>
-
-<!--회원가입-->
-    <insert id="insert">
-    INSERT INTO USERS
-    (ID, IDENTIFICATION, PASSWORD, NAME, PHONENUMBER, ADDRESS, STATUS, ROLL, CREATEDDATE, PROFILEIMAGENAME, PROFILEIMAGESIZE, PROFILEIMAGEUUID, SAVINGLEVEL_ID)
-    VALUES(SEQ_USER.NEXTVAL, #{identification},#{password},#{name},#{phoneNumber},#{address},1,normal, SYSDATE,#{profileImageName},#{profileImageSize},#{profileImageUuid},#{levelId});
-    </insert>
+    };
 
 }
--- src/test/java/com/tikkeul/app/UserMapperTests.java
+++ src/test/java/com/tikkeul/app/UserMapperTests.java
@@ -1,5 +1,6 @@
 package com.tikkeul.app;
 
+import com.tikkeul.app.domain.vo.UserVO;
 import com.tikkeul.app.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -20,9 +21,23 @@ public class UserMapperTests {
     public void adminselectTest(){
       assertThat(userMapper.adminSelectUserAll()).hasSize(0);
     }
+
+
+//  회원가입
     @Test
     public void joinTest(){
-
+        UserVO userVO = new UserVO();
+        userVO.setIdentification("a");
+        userVO.setPassword("b");
+        userVO.setLevelId(1L);
+        userVO.setName("홍홍홍");
+        userVO.setRoll("normal");
+        userVO.setPhoneNumber("010-1231-1123");
+        userVO.setAddress("서울");
+        userVO.setProfileImageName("A");
+        userVO.setProfileImageSize("a");
+        userVO.setProfileImageUuid("a");
+        userMapper.insert(userVO);
     }
 
 }
