7e979e0f77926b27923abdc46544ff54f90f97c0
YunkiHong <>
2023-05-19 14:04:24 +0900

    05-18 회원가입 백엔드 작업중

--- src/main/java/com/tikkeul/app/dao/UserDAO.java
+++ src/main/java/com/tikkeul/app/dao/UserDAO.java
@@ -22,32 +22,16 @@ public class UserDAO {
     //    아이디 중복검사
     public Optional<UserVO> selectByUserId(String identification){
         return userMapper.selectByUserId(identification);
-    };
+    }
 
     //    회원가입
     public void insert(UserVO userVO){
         userMapper.insert(userVO);
-    };
+    }
 
     //    로그인
     public Optional<Long> selectByUserIdAndUserPassword(@Param("id") String id, @Param("password") String password){
         return userMapper.selectByUserIdAndUserPassword(id,password);
-    };<select id="adminSelectUserAll" resultType="userVO">
-        SELECT ID, NAME , IDENTIFICATION, ADDRESS, PHONENUMBER
-        FROM USERS
-    </select>
-
-    <select id="selectByUserId" resultType="UserVO">
-    SELECT ID, IDENTIFICATION, PASSWORD, NAME, PHONENUMBER, ADDRESS, STATUS, ROLL, CREATEDDATE, PROFILEIMAGENAME, PROFILEIMAGESIZE, PROFILEIMAGEUUID, SAVINGLEVEL_ID
-    FROM USERS
-
-    </select>
-
-<!--회원가입-->
-    <insert id="insert">
-    INSERT INTO USERS
-    (ID, IDENTIFICATION, PASSWORD, NAME, PHONENUMBER, ADDRESS, STATUS, ROLL, CREATEDDATE, PROFILEIMAGENAME, PROFILEIMAGESIZE, PROFILEIMAGEUUID, SAVINGLEVEL_ID)
-    VALUES(SEQ_USER.NEXTVAL, #{identification},#{password},#{name},#{phoneNumber},#{address},1,normal, SYSDATE,#{profileImageName},#{profileImageSize},#{profileImageUuid},#{levelId});
-    </insert>
+    }
 
 }
