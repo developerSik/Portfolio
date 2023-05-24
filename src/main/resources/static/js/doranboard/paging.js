776ad9c17bcb76dcbaf234709a9d6bd149276741
Lenovo-dev <cbg050430@gmail.com>
2023-05-24 18:34:30 +0900

    도란도란

--- src/main/resources/static/js/doranboard/paging.js
+++ src/main/resources/static/js/doranboard/paging.js
@@ -6,8 +6,8 @@ $("a.change-page").on("click", function(e){
     location.href = `/doranboard/doranboard?page=${page}&type=${type}&keyword=${keyword}`;
 });
 
-$(".sub-category-list").on("click", function(e){
+/*$(".sub-category-list").on("click", function(e){
     e.preventDefault();
     let item = $("button[name=item]").val();
     location.href = `/doranboard/doranboard?item=${item}`;
-});
+});*/
