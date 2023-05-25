9ab2140bdf0ea14a106e9c97d08fbc77474e7413
Yunkihong-Dev <81303136+Yunkihong-Dev@users.noreply.github.com>
2023-05-25 17:30:45 +0900

    Delete build 2 directory

--- build 2/resources/main/static/js/doranboard/paging 3.js
+++
@@ -1,7 +0,0 @@
-$("a.change-page").on("click", function(e){
-    e.preventDefault();
-    let page = $(this).attr("href");
-    let type = $("select[name=type]").val();
-    let keyword = $("input[name=keyword]").val();
-    location.href = `/doranboard/doranboard?page=${page}&type=${type}&keyword=${keyword}`;
-});
--- build 2/resources/main/static/js/mypage/mytikkle 3.js
+++
@@ -1,8 +0,0 @@
-/*
-* mytikkle.html
-* */
-
-$(".MuiTypography-root").on("click", function(){
-    $(".MuiTypography-root").removeClass("css-eskagx");
-    $(this).addClass("css-eskagx");
-});
